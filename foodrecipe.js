2ecc5ebdd2dfd73de77cacabb29a6ada945430ef
Maria (Hye Ryeong) Shin <hrs284@nyu.edu>
2018-02-05 14:55:40 -0500

    Delete foodrecipe.js

--- foodrecipe.js
+++
@@ -1,6 +0,0 @@
-unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=Spaghetti+Aglio+et+Olio")
-.header("X-Mashape-Key", "wV1j1qbVSPmshY5JE6GuTbGoS3fRp1M24idjsnWKu24B4FwzDv")
-.header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
